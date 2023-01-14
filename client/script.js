2d86de779dbea42c40bab8d3cdeffc31ee19e5ea
IsraelBosun <israelbosun1@gmail.com>
2023-01-14 10:05:44 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://afro-gpt1.onrender.com', {
     method: "POST",
     headers: {
       'Content-Type': 'application/json'
